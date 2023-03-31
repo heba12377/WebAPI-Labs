b527a4ee118f05a3a8a040c6a4a73860398efd36
heba12377 <hebaelsaidali@gmail.com>
2023-03-31 17:49:21 +0200

    added lab 2

---
+++ Day2/Day2.PL/Managers/ITicketsManager.cs
@@ -0,0 +1,15 @@
+﻿using Day2.BL.Dtos.Tickets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2.BL.Managers
+{
+    public interface ITicketsManager
+    {
+        List<TicketReadDto> GetAll();
+        void Add(TicketAddDto ticketDto);
+    }
+}
